a961f8ba68d4d94f6f4241070b9267a6e11b1456
WELCOME RAP & NRP <najuarahmahputri@gmail.com>
2024-11-12 11:58:55 +0700

    Viewdata

---
+++ app/src/main/java/najuarahmahputri/polbeng/ac/id/viewdata/MainActivity.kt
@@ -0,0 +1,26 @@
+package najuarahmahputri.polbeng.ac.id.viewdata
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import najuarahmahputri.polbeng.ac.id.viewdata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnViewList.setOnClickListener{
+            startActivity(Intent(applicationContext,
+                ListviewActivity::class.java))
+        }
+        binding.btnRecylerView.setOnClickListener{
+            startActivity(Intent(applicationContext,
+                RecylerActivity::class.java))
+        }
+    }
+}
